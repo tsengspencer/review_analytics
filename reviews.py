da8f4552063b0ef456dc266998e3eb34bd527a22
Specner Tseng <yuyun@gapp.nthu.edu.tw>
2022-02-14 20:47:25 +0800

    selection

--- reviews.py
+++ reviews.py
@@ -10,3 +10,12 @@ with open("reviews.txt","r") as f:
 				print(len(data)) #確保讀取進度
 print("檔案讀取完畢", "總共有", len(data), "筆資料")
 print("每筆留言的平均長度為", sum_length/len(data))
+
+# 篩選字數小於100的留言
+new = []
+for review in data:
+	if len(review) < 100:
+		new.append(review)
+print("一共有",len(new),"個留言的字數小於100") 
+print(new[0]) #隨機查看字數小於100的留言
+print(new[1])
